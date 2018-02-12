40899fbeb7f7d180f2dd08ef16360da10e4688b4
Adrien <adrien.castellon@etu.univ-lyon1.fr>
2018-02-12 12:10:33 +0100

    Ajout nodeJS, projet express

---
+++ backEnd/server.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let app = express()  // Charge express
+
+app.set('view engine','ejs') // set ejs as 'html' view engine
+
+app.use('/assets',express.static('public')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
+app.use(express.static('images'))
+app.get('/', function (request,response) {
+    response.render('pages/index',{test: 'Salut'})
+
+})
+
+app.listen(8080)
