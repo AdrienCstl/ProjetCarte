6476b3b961a48fc375655ff7573c64bd2295fd19
Adrien <adrien.castellon@etu.univ-lyon1.fr>
2018-02-13 15:12:30 +0100

    Ignore node_modul

--- site/server.js
+++ site/server.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let app = express()  // Charge express
 
-app.set('view engine','ejs') // set ejs as 'html' view engine
+//app.set('view engine','ejs') // set ejs as 'html' view engine
 
 app.use('/assets',express.static('public')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
 app.use(express.static('images'))
