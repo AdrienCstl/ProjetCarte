d336dd909dc834d8716729420bb2b28cdec17ab8
Adrien <adrien.castellon@etu.univ-lyon1.fr>
2018-02-13 17:08:17 +0100

    configuration du front end avec express

--- site/server.js
+++ site/server.js
@@ -1,18 +1,19 @@
 let express = require('express')
 let app = express()  // Charge express
 
-let db = require('script.je')
+let db = require('./script.js')
 
 
 app.set('view engine','ejs') // set ejs as 'html' view engine
 
-app.use('/assets',express.static('public')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
-app.use(express.static('images'))
-app.set('views', path.join(__dirname, './'))
+app.use(express.static('frontEnd')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
 app.get('/', function (request,response) {
-    response.render('frontEnd/index')
+    response.render('../frontEnd/index')
     db.connection()
 })
 
 app.listen(8080)
+
+
+
 
