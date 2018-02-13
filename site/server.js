153fa1d319590cff9510033e8255005bfb227841
Adrien <adrien.castellon@etu.univ-lyon1.fr>
2018-02-13 17:38:35 +0100

    connection BD

--- site/server.js
+++ site/server.js
@@ -6,10 +6,12 @@ let db = require('./script.js')
 
 app.set('view engine','ejs') // set ejs as 'html' view engine
 
-app.use(express.static('frontEnd')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
+//app.use(express.static('frontEnd')) //define the route for everything in frontEnd. but need the prefix /assets (not needed but interrested)
 app.get('/', function (request,response) {
-    response.render('../frontEnd/index')
+
     db.connection()
+
+    response.render('../frontEnd/index')
 })
 
 app.listen(8080)
