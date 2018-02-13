bd12dbdf6ceb0461cbc0074da79f1d648470d4ce
Adrien <adrien.castellon@etu.univ-lyon1.fr>
2018-02-13 16:30:22 +0100

    ajout du lien pour la connection

--- site/server.js
+++ site/server.js
@@ -1,13 +1,18 @@
 let express = require('express')
 let app = express()  // Charge express
 
-//app.set('view engine','ejs') // set ejs as 'html' view engine
+let db = require('script.je')
+
+
+app.set('view engine','ejs') // set ejs as 'html' view engine
 
 app.use('/assets',express.static('public')) //define the route for everything in public. but need the prefix /assets (not needed but interrested)
 app.use(express.static('images'))
+app.set('views', path.join(__dirname, './'))
 app.get('/', function (request,response) {
     response.render('frontEnd/index')
-
+    db.connection()
 })
 
 app.listen(8080)
+
