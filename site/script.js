91134d114467025b038ec5438a3d7cc7a3e0541b
Thomas Broutier <thomas.broutier@gmail.com>
2018-02-26 10:24:42 +0100

    Ajout de script

--- site/script.js
+++ site/script.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 
 
 exports.connection = function (){
-    console.log("test");
     mongoose.connect('mongodb://projetcarte_test:test@mongodb-projetcarte.alwaysdata.net:27017/projetcarte_bd');
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
@@ -12,3 +11,16 @@ exports.connection = function (){
         console.log("connection succesfull");
     });
 }
+
+exports.getData = function() {
+	
+	
+	
+	
+}
+
+
+var schema = new mongoose.Schema({ type: 'string', nom: 'string', adresse: 'string',
+codepostal:'string', email: 'string', geometry: {coordinates[0:'Number', 1:'Number']}
+  });
+var Tank = mongoose.model('Tank', schema);
