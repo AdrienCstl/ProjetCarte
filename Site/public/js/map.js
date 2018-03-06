944420f1fbcb23d721ad67e3c5899a6d8a3863a1
AdrienCstl <adrien.castellon@etu.univ-lyon1.fr>
2018-03-06 20:27:25 +0100

    ajout des info des pin sur le popup

--- Site/public/js/map.js
+++ Site/public/js/map.js
@@ -22,19 +22,18 @@ map.addControl(new mapboxgl.NavigationControl());
 map.scrollZoom.disable();
 
 
-
 //add markers to map
 
-function refresh(){
+function refresh() {
     var compteur = 0;
-    geojson.features.forEach(function(marker) {
-    // create a HTML element for each feature
+    geojson.features.forEach(function (marker) {
+        // create a HTML element for each feature
         var el = document.createElement('div');
         el.className = 'marker';
         el.addEventListener("click", afficherPopup);
-       // el.toggleClass = 'infoPin';
+        // el.toggleClass = 'infoPin';
 
-    // make a marker for each feature and add to the map
+        // make a marker for each feature and add to the map
         new mapboxgl.Marker(el)
             .setLngLat(marker.geometry.coordinates)
             .addTo(map);
@@ -49,28 +48,64 @@ function refresh(){
 
 function afficherPopup(event) {
 
-
     var elem = data[event.target.id];
-    $(".infoPin").toggle;
+    //afficheDetail(elem);
+    var tmpDivItem = document.createElement('div');
+    var tmpDivContent = document.createElement('div');
+    var tmpDivMeta = document.createElement('div');
+    var tmpDivDesc = document.createElement('div');
+    var tmpDivExtra = document.createElement('div');
+    var tmpSpan = document.createElement('span');
+    var tmpP= document.createElement('p');
+    var tmpH = document.createElement('a');
 
+    tmpDivItem.style.display = "none";
+    tmpDivItem.style.whiteSpace = "initial";
+    tmpDivItem.style.overflow = "hidden";
+    tmpDivItem.style.maxWidth = "253px";
+
+    tmpDivItem.className = 'item';
+    tmpDivItem.id = 'popup';
+    tmpH.className = "ui small header";
+    tmpDivMeta.className = "meta";
+    tmpDivDesc.className = "description";
+    tmpDivExtra.className = "extra";
+    tmpDivContent.className = "content";
+
+
+
+
+    tmpDivItem.append(tmpDivContent);
+        tmpDivContent.append(tmpH);
+            tmpH.append(elem.nom);
+        tmpDivContent.append(tmpDivMeta);
+            tmpDivMeta.append(tmpSpan);
+                tmpSpan.innerHTML = "<i class=\"address book outline icon\"></i>" + elem.adresse + "<br>" +"&nbsp&nbsp&nbsp&nbsp&nbsp"+ elem.codepostal + "<br />"  +"&nbsp&nbsp&nbsp&nbsp&nbsp"+ elem.commune +"<br><br>"+ "<i class=\"phone icon\"></i> "+ elem.telephone ;
+        tmpDivContent.append(tmpDivDesc);
+            tmpDivDesc.append(tmpP);
+        //tmpP.append(elem.adresse + bra + elem.codepostal + "<br />"  + elem.commune );
+        tmpDivContent.append(tmpDivExtra);
+
+    $("#navMap")[0].firstElementChild.replaceChild(tmpDivItem, $("#navMap")[0].firstElementChild.lastElementChild);
+    $("#popup").slideToggle("fast");
 
 }
 
 function addMarkerstoList(features, data) {
     let currentSize = features.length;
-    for(var i = currentSize; i <  window.data.length + currentSize; i++){
+    for (var i = currentSize; i < window.data.length + currentSize; i++) {
         features[i] = {};
         features[i].type = "Feature";
-        features[i].properties = {message: data[i-currentSize].type};
-        features[i].geometry = data[i-currentSize].geometry;
+        features[i].properties = {message: data[i - currentSize].type};
+        features[i].geometry = data[i - currentSize].geometry;
     }
 
 
 }
 
 //Requetes AJAX
-$.post( "/data" ,function(d) {
-    $( ".result" ).html( d );
+$.post("/data", function (d) {
+    $(".result").html(d);
     data = d;
     console.log(data);
 
@@ -78,3 +113,37 @@ $.post( "/data" ,function(d) {
     console.log(geojson.features);
     refresh();
 });
+
+
+function afficheDetail(elem) {
+    var premiereligne = $(".row")[0];
+    var deuxiemeligne = $(".row")[1];
+
+    //Champs à ajouter
+    var titre = "<h3>" + elem.nom + "</h3>";
+    var type = "<p>" + elem.type + "</p>";
+    var description = "<p>Y a rien</p>";
+
+    var horaire = "<p>" + elem.ouverture + "</p>";
+
+    var adresse = "<p>" + adresse + " " + elem.codepostal + " " + elem.commune + "</p>";
+    var contact = "<p>" + elem.telephone + "</p><p>" + elem.email + "</p>";
+
+
+    //Ajout dans le template
+
+    //Ajout dans la premiere ligne
+    //Ajout dans la deuxieme colonne
+    premiereligne.getElementsByClassName(".thirteen wide column")[0].append(titre);
+    premiereligne.getElementsByClassName(".thirteen wide column")[0].append(type);
+    premiereligne.getElementsByClassName(".thirteen wide column")[0].append(description);
+
+    //Ajout dans la deuxieme ligne
+    //Ajout dans la premiere colonne
+    deuxiemeligne.getElementsByClassName('.three wide column')[0].append(horaire);
+
+    //Ajout dans la deuxieme colonne
+    deuxiemeligne.getElementsByClassName(".ten wide column")[0].append(adresse);
+    deuxiemeligne.getElementsByClassName(".ten wide column")[0].append(contact);
+
+}
